38fbeb15caba255d76a5557c8b9365cbdf8c5a50
Suraj Murmu <48622574+SurajMurmu7552@users.noreply.github.com>
2021-01-21 13:06:01 +0530

    form done

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./register.css"
 
 export default function Register() {
-    const handleValidation = ()=>{
+
+    let [err , setErr] = useState({});
+    let [values, setValues] = useState({firstname: "First name" , lastname : "Last name", email :"Email" , password : "Password"});
+
+    const handleValidation = (temp)=>{
+        let error = {}
+        let valid =true;
+
+        if(temp.querySelector('#firstname').value === null || temp.querySelector('#firstname').value=== undefined || temp.querySelector('#firstname').value ==="" ){   
+            valid = false;
+            temp.querySelector('#firstname').parentNode.classList.add('error');
+            error["firstname"] = "First Name cannot be empty";
+        }
+        if(temp.querySelector('#lastname').value === null || temp.querySelector('#lastname').value=== undefined || temp.querySelector('#lastname').value ==="" ){
+            
+            valid = false;
+            temp.querySelector('#lastname').parentNode.classList.add('error');
+            error["lastname"] = "Last Name cannot be empty";
+        }
+        if(temp.querySelector('#email').value === null || temp.querySelector('#email').value=== undefined || temp.querySelector('#email').value ==="" ){
+
+            valid = false;
+            temp.querySelector('#email').parentNode.classList.add('error');
+            temp.querySelector('#email').placeholder= "email@example/com";
+            error["email"] = "Email cannot be empty";
+        }
+        if(temp.querySelector('#password').value === null || temp.querySelector('#password').value=== undefined || temp.querySelector('#password').value ==="" ){
+
+            valid = false;
+            temp.querySelector('#password').parentNode.classList.add('error');
+            error["password"] = "Password cannot be empty";
+        }
+        
+        setErr(err = error);
+        return valid;
 
     }
-    const handleInput=()=>{
+
+    const onSubmit =(e) =>{
+        e.preventDefault();
+        const valid = handleValidation(e.target);
+        console.log(valid)
+        if(valid)   alert('form submitted successfully')
+    }
+
+    const onClick =(e) =>{
+        e.target.parentNode.classList.remove('error');
 
     }
+    
+    const handleChange = (e)=>{
+
+        const obj = values
+        obj[e.target.id] =e.target.value
+        setValues(values = obj)
+        console.log(values)
+    }
+
     return (
         <div className="register-card">
-            <form className="card" >
-                <input className="card-item error" type="text" name="firstname" placeholder="First Name" />
-                <input className="card-item"type="text" name="lastname" placeholder="Last Name"/>
-                <input className="card-item" type="email" name="email" placeholder="Email Address"/>
-                <input className="card-item" type="password" name="password" placeholder="Password"/>
+            <form className="card" onSubmit={onSubmit} >
+                <div className="card-item">
+                    <input  type="text" id="firstname" placeholder= {values['firstname']} onChange={handleChange} onClick={onClick} />
+                    <span className="icon"><img src="http://localhost:3000/asset/icon-error.svg" alt=""/></span>
+                    <span className='card-err' style={{ color: "red"}}>{err['firstname']}</span> 
+                </div>  
+                <div className="card-item">
+                    <input type="text" id="lastname" placeholder={values['lastname']} onChange={handleChange} onClick={onClick}/>
+                    <span className="icon"><img src="http://localhost:3000/asset/icon-error.svg" alt=""/></span>
+                    <span className='card-err' style={{ color: "red"}}>{err['lastname']}</span> 
+                </div> 
+                <div className="card-item">
+                    <input type="text" id="email" placeholder={values['email']} onChange={handleChange} onClick={onClick} />
+                    <span className="icon"><img src="http://localhost:3000/asset/icon-error.svg" alt=""/></span>
+                    <span className='card-err' style={{ color: "red"}}>{err['email']}</span> 
+                </div> 
+                <div className="card-item">
+                    <input type="text" id="password" placeholder={values['password']} onChange={handleChange} onClick={onClick} />
+                    <span className="icon"><img src="http://localhost:3000/asset/icon-error.svg" alt=""/></span>
+                    <span className='card-err' style={{ color: "red"}}>{err['password']}</span> 
+                </div> 
                 <input className="card-item submit" type="submit" value="CLAIM YOUR FREE TRIAL" />
             </form>
            <div className="terms">
